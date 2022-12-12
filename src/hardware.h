7cbe1f71ed9af084f4e4e2da037b2d12f7ac7052
Deepak Khajuria <9539519+dk307@users.noreply.github.com>
2022-12-11 20:53:23 -0800

    Basic History of sensors

--- src/hardware.h
+++ src/hardware.h
@@ -2,6 +2,7 @@
 
 #include "sensor.h"
 #include "task_wrapper.h"
+#include "psram_allocator.h"
 
 class hardware
 {
@@ -12,21 +13,29 @@ public:
 
     static hardware instance;
 
-    sensor_value &get_sensor(sensor_id_index index)
+    const sensor_value &get_sensor(sensor_id_index index)
     {
         return sensors[static_cast<uint8_t>(index)];
+    }
+
+    const sensor_history &get_sensor_history(sensor_id_index index)
+    {
+        return (*sensors_history)[static_cast<uint8_t>(index)];
     }
 
 private:
     hardware() = default;
 
-    const static std::array<sensor_definition_display, 6> aqi_definition_display;
-    const static std::array<sensor_definition_display, 1> voc_definition_display;
-    const static std::array<sensor_definition_display, 1> co2_definition_display;
-    const static std::array<sensor_definition_display, 1> temperature_definition_display;
-
     // same index as sensor_id_index
     std::array<sensor_value, total_sensors> sensors;
+    std::unique_ptr<std::array<sensor_history, total_sensors>, psram::deleter> sensors_history;
 
     std::unique_ptr<task_wrapper> sensor_read_task;
+
+    template <class T>
+    void set_sensor_value(sensor_id_index index, T value)
+    {
+        const auto new_value = sensors[static_cast<size_t>(index)].set_value(value);
+        (*sensors_history)[static_cast<size_t>(index)].add_value(new_value);
+    }
 };
